941aebaa24c276f4bc84c8b4c84d699590c5c19b
LenaMatyjasik <lmw@technetconsulting.no>
2024-06-15 14:39:10 +0200

    init commit

---
+++ src/test/java/com/ekea/demo/EkeaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkeaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
